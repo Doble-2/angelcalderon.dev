f1425fa2ffb4316dfca89ba20db08b8ee2e12deb
Angel Calderon <angelcalderon303@gmail.com>
2024-09-08 19:20:35 -0400

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output:  "hybrid",
-  adapter: vercel(),
   integrations: [tailwind(), icon()]
 });
