83fe9104a6226da04360fdbea12d7ef9976975ce
Angel Calderon <angelcalderon303@gmail.com>
2025-08-31 21:31:04 -0400

    feat: agregar lucide-react y mejorar secciones de la interfaz

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				// small rotation wiggle
+			
+				// vertical bounce
+				minbounce: {
+					'0%, 100%': {
+						transform: 'translateY(0)',
+						animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+					},
+					'50%': {
+						transform: 'translateY(-05%)',
+						animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+					},
+				},
+			},
+			animation: {
+				minbounce: 'minbounce 1s infinite'
+			},
+		},
 	},
 	plugins: [],
 }
